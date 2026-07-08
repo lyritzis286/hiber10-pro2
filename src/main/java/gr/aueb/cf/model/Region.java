7b2c4463e1f56df89bcfc4fdf71f17d32d265c8f
lyritzis286 <lyritzis286@gmail.com>
2026-07-08 13:32:46 +0300

    add Region entity with JPA annotations

---
+++ src/main/java/gr/aueb/cf/model/Region.java
@@ -0,0 +1,23 @@
+package gr.aueb.cf.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "regions")
+public class Region {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String title;
+
+}

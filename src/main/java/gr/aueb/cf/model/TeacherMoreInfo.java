b126e7db86891fad303701a1087bb3971da3cc4a
lyritzis286 <lyritzis286@gmail.com>
2026-07-08 13:39:27 +0300

    add TeacherMoreInfo entity with date of birth and gender fields

---
+++ src/main/java/gr/aueb/cf/model/TeacherMoreInfo.java
@@ -0,0 +1,28 @@
+package gr.aueb.cf.model;
+
+
+import gr.aueb.cf.enums.GenderType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "teacher_more_info")
+public class TeacherMoreInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date_of_birth", nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Enumerated(EnumType.STRING)
+    private GenderType gender;
+}
